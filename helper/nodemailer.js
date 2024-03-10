4ce50835140ba736edeec5490bd1030dfc8f7774
jonty1231 <jontythakur68@gmail.com>
2024-03-09 23:46:23 -0800

    last

--- helper/nodemailer.js
+++ helper/nodemailer.js
@@ -6,19 +6,19 @@ const transport= nodemailer.createTransport({
   // port: 465,
   // secure: true,
     auth: {
-      user: "tip.top.kapil.01@gmail.com",
-      pass: "wipxouladdooceuz",
+      user: "0.baat.cheet.0@gmail.com",
+      pass: "ycrxpwwrmmcwetzz",
     }
 })
 
  export const sendMail=(mail,otp)=>{
-const details={ from:"tip.top.kapil.01@gmail.com",
+const details={ from:"0.baat.cheet.0@gmail.com",
 to:mail,
 subject:"verify",
 html:`<div style="height:100vh;width:100%; background:rgb(34, 190, 3); text-align: center ;padding: 2rem  ;
 display: flex; justify-content: center; align-items: center;"><div>
- <b style="font-size: 4rem;">T I P T O P</b>
- <p style="font-size: 2rem;">Please use the verification code below on the TIPTOP Website</p>
+ <b style="font-size: 4rem;">K U M L I</b>
+ <p style="font-size: 2rem;">Please use the verification code below on the KHUMLI Website</p>
  <p style="font-size: 4rem; color: rgb(252, 36, 3); letter-spacing: 1rem;">${otp}</p>
  <p class="font-size:1rem;">Thanks!</p>
 </div>
